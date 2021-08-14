ad8ceb12bef198ec098eab1a0139187958b82cad
Anna Weber <annaweberdev@gmail.com>
2021-08-14 16:59:24 -0700

    complete hw 3 part 2 and reorganize files

---
+++ mc-package/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="mcsim",
+    version="0.0.1",
+    author="Anna Weber",
+    author_email="annaweberdev@gmail.com",
+    description="A package for running a Monte Carlo simulation",
+    long_description="A Python package which performs MC simulation using the python standard libraries.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/USERNAME/REPONAME",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.9",
+)
